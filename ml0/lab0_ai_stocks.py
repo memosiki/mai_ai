f9e4b02f0ac6a18c4ccf454afd8dd7c2f867daa9
memosiki <russiankoteyka@gmail.com>
2019-06-20 00:26:34 +0300

    Сделать лабораторную на Azure ML

---
+++ ml0/lab0_ai_stocks.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("^IXIC.csv")
+print(df.columns)
+df.plot.scatter(x='Open', y='High')
+df.plot.scatter(x='High', y='Adj Close')
+df.plot.scatter(x='Open', y='Adj Close')
+
+adj_price = df["Adj Close"]
+print("Характеристики для скорректированной цены")
+print("Среднее значение", adj_price.mean())
+print("Медиана", adj_price.median())
+print("Мода")
+{print(val) for val in df["Adj Close"].mode().values}
+print("Максимум", adj_price.max())
+print("Минимум", adj_price.min())
+print("Размах", adj_price.max()-adj_price.min())
+print("Стандартное отклонение", adj_price.std())
+print('Дисперсия', adj_price.std()**2)
+plt.show()
